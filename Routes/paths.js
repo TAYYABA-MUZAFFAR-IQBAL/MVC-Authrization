09413923629911b8463b7d538beaf0163a9c0aed
TAYYABA <tayyaba.tahira98@gmail.com>
2022-06-20 12:44:23 +0500

    ControllersWithClassApproch

--- Routes/paths.js
+++ Routes/paths.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const sigupController= require("../Controllers/employeeController");
-const LoginController= require("../Controllers/employeeLoginController");
-const AuthController= require("../Controllers/AuthrizeAdminEmployeeController");
-const  mustBeLoggedIn  = require("../Middleware/MustBeLoginMiddleware");
-const router= express.Router();
+// const sigupController= require("../Controllers/employeeController");
+// const LoginController= require("../Controllers/employeeLoginController");
+// const AuthController= require("../Controllers/AuthrizeAdminEmployeeController");
+// const FuncController = require("../Controllers/FunctionalController");
+const mustBeLoggedIn = require("../Middleware/MustBeLoginMiddleware");
+const router = express.Router();
+const controllers=require("../Controllers/FunctionalController")
 
-
-router.route("/Signup").post(sigupController);
-router.route("/Login").post(LoginController);
-router.route("/GetAcessToAllRecord").get(mustBeLoggedIn,AuthController);
+router.route("/Signup").post(controllers.test.registerEmployee);
+router.route("/Login").post(controllers.test.LoginEmployee);
+router
+  .route("/GetAcessToAllRecord")
+  .get(mustBeLoggedIn, controllers.test.GetAllEmployee);
 
 module.exports = router;
